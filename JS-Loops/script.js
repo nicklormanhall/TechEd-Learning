cd8178cab04eadde3104b2c297380dc15cf4e72b
nicklormanhall <nicklormanhall@gmail.com>
2024-03-25 22:37:39 +0000

    JS-Loops

---
+++ JS-Loops/script.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("Iteration", i);
+// }
+
+let counter = 1;
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+
+// let keepLooping = true;
+// let i = 0;
+// while (keepLooping === true) {
+//   console.log("Iteration", i);
+//   // set keepLooping to false with 10% chance: Math.random() returns a random value between 0 and 1
+//   keepLooping = Math.random() < 0.1;
+//   i++;
+// }
+
+// console.log(keepLooping);
