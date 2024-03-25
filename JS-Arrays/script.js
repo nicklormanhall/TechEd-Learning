cba15e9fa5a3e442476123740ac29fc02cb9cf94
nicklormanhall <nicklormanhall@gmail.com>
2024-03-25 15:29:00 +0000

    JS-Arrays

---
+++ JS-Arrays/script.js
@@ -0,0 +1,24 @@
+console.log("Hello world!");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+console.log(blogPosts);
+
+console.log(blogPosts[0]); // Output: My first blog post
+console.log(blogPosts[1]); // Output: My second blog post
+console.log(blogPosts[2]); // Output: My third blog post
+
+blogPosts[3] = "My fourth blog post";
+console.log(blogPosts[3]); // Output: My fourth blog post
+
+const favouriteFoods = ["chocolate", "jaffa cakes", "sausages", "chilli"];
+const favouriteColours = ["red", "white", "blue"];
+const favouriteNumbers = [20, 6, 7, 11];
+
+console.log(favouriteFoods);
+console.log(favouriteColours);
+console.log(favouriteNumbers);
