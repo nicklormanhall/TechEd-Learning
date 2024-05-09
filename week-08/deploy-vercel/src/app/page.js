630be55ab48dfcd800ca7c11897c3b4fad32e5da
nicklormanhall <nicklormanhall@gmail.com>
2024-05-09 10:23:01 +0100

    Vercel deployment

---
+++ week-08/deploy-vercel/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Deploying to Vercel</h1>
+      <p>This is deploying the vercel</p>
+    </div>
+  );
+}
